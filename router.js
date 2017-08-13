fb2a019ca63e5576f08eda61aa7643b6666ec567
mumu111111 <2646709035@qq.com>
2017-08-13 09:52:18 +0800

    ajax

---
+++ router.js
@@ -0,0 +1,18 @@
+// function setRouter(app) {
+//     var router = app;
+//
+//
+//
+        router.get('/loadmore',function(req,res){
+        var curIdx= req.query.index
+        var len= req.query.length
+        var data=[]
+        for(var i=0;i<len;i++){
+            data.push('内容'+(parseInt(curIdx)+i))
+        }
+        res.send(data)
+    })
+
+}
+//     router.get('/loadMore', function(req, res) {
+//     res.send(['ruoyu','dddd','dddddd','dadada','dadadada'])})
